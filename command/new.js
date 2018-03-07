d6cda8c99bea1eb1bad9047cba03a9a8e7222ddb
Mrlyjoutlook <mr.lyj@outlook.com>
2018-03-07 09:00:12 +0800

    release 0.6.0

--- command/new.js
+++ command/new.js
@@ -9,59 +9,78 @@ const inquirer = require('inquirer');
 
 const arg = process.argv[2];
 if (!arg) {
-  console.log(chalk.red('peak: 缺少项目工程名称'))
+  console.log(chalk.red('peak: 缺少项目工程名称'));
 } else {
   console.log(chalk.green('peak: building...'));
-  inquirer.prompt([{
-    type: 'list',
-    message: 'peak: select the type of development technology',
-    name: 'react',
-    choices: [
+  inquirer
+    .prompt([
       {
-        key: 'redux',
-        name: 'react+redux',
-        value: 'redux',
-      },
-      {
-        key: 'mbox',
-        name: 'react+mbox',
-        value: 'mbox',
-      },
-    ],
-  }])
-    .then(function (firstAnswers) {
-
-      inquirer.prompt([{
         type: 'list',
-        message: 'peak: select language ts(typescript) or js(javascript)',
-        name: 'language',
+        message: 'peak: select the type of development technology',
+        name: 'react',
         choices: [
           {
-            key: 'ts',
-            name: 'typescript(推荐使用typescript开发)',
-            value: 'ts',
+            key: 'redux',
+            name: 'react+redux',
+            value: 'redux',
           },
           {
-            key: 'js',
-            name: 'javascript',
-            value: 'js',
+            key: 'mobx',
+            name: 'react+mobx',
+            value: 'mobx',
           },
         ],
-      }])
-        .then(function (twoAnswers) {
+      },
+    ])
+    .then(function(firstAnswers) {
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            message: 'peak: select language ts(typescript) or js(javascript)',
+            name: 'language',
+            choices: [
+              {
+                key: 'ts',
+                name: 'typescript(推荐使用typescript开发)',
+                value: 'ts',
+              },
+              {
+                key: 'js',
+                name: 'javascript',
+                value: 'js',
+              },
+            ],
+          },
+        ])
+        .then(function(twoAnswers) {
           const dest = path.join(process.cwd(), arg);
-          const boilerplates = twoAnswers.language === 'ts' ? `${firstAnswers.react}-ts` : firstAnswers.react
+          const boilerplates =
+            twoAnswers.language === 'ts'
+              ? `${firstAnswers.react}-ts`
+              : firstAnswers.react;
           const cwd = path.join(__dirname, '../boilerplates/', boilerplates);
           mkdirpSync(dest);
-          writeJsonSync(dest + '/peak.json', Object.assign(
-            require(path.join(__dirname, '../peak.json')),
-            { language: twoAnswers.language }
-          ));
-          copySync(path.join(__dirname, '../config'), dest + '/config', { overwrite: true });
-          copySync(path.join(__dirname, '../bin'), dest + '/bin', { overwrite: true });
-          vfs.src(['**/*', '!node_modules/**/*'], { cwd: cwd, cwdbase: true, dot: true })
+          writeJsonSync(
+            dest + '/peak.json',
+            Object.assign(require(path.join(__dirname, '../peak.json')), {
+              language: twoAnswers.language,
+            })
+          );
+          copySync(path.join(__dirname, '../config'), dest + '/config', {
+            overwrite: true,
+          });
+          copySync(path.join(__dirname, '../bin'), dest + '/bin', {
+            overwrite: true,
+          });
+          vfs
+            .src(['**/*', '!node_modules/**/*'], {
+              cwd: cwd,
+              cwdbase: true,
+              dot: true,
+            })
             .pipe(vfs.dest(dest))
-            .on('end', function () {
+            .on('end', function() {
               console.log(chalk.green('peak: building ok'));
             });
         });
--- config/README.md
+++ config/README.md
@@ -1,10 +1,11 @@
 # App Config
+
 项目所有的执行文件的配置会读取根目录下的`peak.json`文件。
 
 ## Directory
-- env
-- server
-- sw
-- utils
-- webpack
 
+* env 主要存放环境变量
+* server 服务器
+* sw `service work`配置文件
+* utils 工具库
+* webpack webpack 配置文件
